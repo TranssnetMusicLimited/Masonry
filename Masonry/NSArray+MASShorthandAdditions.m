a42da44c5e9784d1a211249461ea0be99c83b92c
frank-du <ion.fundo@gmail.com>
2019-10-09 17:27:22 +0800

    remove a mess of warnings

---
+++ Masonry/NSArray+MASShorthandAdditions.m
@@ -0,0 +1,29 @@
+//
+//  NSArray+MASShorthandAdditions.m
+//  Masonry iOS
+//
+//  Created by frank-du on 2019/10/9.
+//  Copyright © 2019 Jonas Budelmann. All rights reserved.
+//
+
+#import "NSArray+MASShorthandAdditions.h"
+
+#ifdef MAS_SHORTHAND
+
+@implementation NSArray (MASShorthandAdditions)
+
+- (NSArray *)makeConstraints:(void(^)(MASConstraintMaker *))block {
+    return [self mas_makeConstraints:block];
+}
+
+- (NSArray *)updateConstraints:(void(^)(MASConstraintMaker *))block {
+    return [self mas_updateConstraints:block];
+}
+
+- (NSArray *)remakeConstraints:(void(^)(MASConstraintMaker *))block {
+    return [self mas_remakeConstraints:block];
+}
+
+@end
+
+#endif
